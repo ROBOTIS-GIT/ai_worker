d9a6588d4b4362ed7fb9e826fa4ed13555371ad7
Hyunwoo Nam <nhw@robotis.com>
2026-07-21 19:11:09 +0900

    Updated SH5 ros2_control and urdf xacro
    Signed-off-by: Hyunwoo Nam <nhw@robotis.com>

--- ffw_bringup/launch/ffw_sh5_follower_ai.launch.py
+++ ffw_bringup/launch/ffw_sh5_follower_ai.launch.py
@@ -164,6 +164,8 @@ def generate_launch_description():
             'hand_r_controller',
             'effort_l_controller',
             'effort_r_controller',
+            'pressure_l_broadcaster',
+            'pressure_r_broadcaster',
             'ffw_robot_manager',
         ],
         parameters=[robot_description],
