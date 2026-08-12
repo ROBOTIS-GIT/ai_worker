b0c00b67b171b369db297d5a73e551273363ac80
YoungWook0533 <kyu@robotis.com>
2026-08-12 06:39:22 +0000

    Refactor launch files for F1 and F2 robots

--- ffw_bringup/launch/ffw_f1_ai.launch.py
+++ ffw_bringup/launch/ffw_f1_ai.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
 
     follower = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(bringup_launch_dir,
-                                                   'f1_follower_ai.launch.py')),
+                                                   'ffw_f1_follower_ai.launch.py')),
         launch_arguments={
             'launch_cameras': 'true',
             'init_position': 'true',
